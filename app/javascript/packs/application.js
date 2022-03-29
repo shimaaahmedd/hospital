aaf5196e8ce274ecd42de35f51366006b0b067f1
shimaaahmedd <shimaahmedd@gmail.com>
2022-03-29 12:36:18 +0200

    some configs configs 1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,16 +14,3 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import Vue from '../config/webpack/loaders/vue.js'
-import App from '../app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('app'))
-  const app = new Vue({
-    el: 'app',
-    template: '<App/>',
-    components: { App }
-  })
-
-  console.log(app)
-})
